25cc3be07ebb37f850fa0b3206a5c72b7a17b31e
orbis <infectores@gmail.com>
2023-03-23 15:37:52 +0300

    validation

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,6 @@
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(
+  OmitType(CreateCoffeeDto, ['id']),
+) {}
