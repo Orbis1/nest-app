b9051a563c4de6305118dc02daf861283236fb07
orbis <infectores@gmail.com>
2023-03-23 11:11:27 +0300

    CoffeeController

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // findAll(@Res() response) {
+    // response.status(200).send('This action returns all coffees');
+    const { limit, offset } = paginationQuery;
+    return (
+      'This action returns all coffees' +
+      ` (limit: ${limit}, offset: ${offset})`
+    );
+  }
+
+  @Get(':id')
+  // findOne(@Param() params) {
+  // const {id} = params;
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+}
