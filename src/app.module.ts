dc588480e3c01808838cd0523034c234b910fb49
orbis <infectores@gmail.com>
2023-03-26 21:59:41 +0300

    separated data source conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { PostgresDataSource } from './app.datasource';
 
 @Module({
-  imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true, //disable for prod
-    }),
-  ],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(PostgresDataSource.options)],
   controllers: [AppController],
   providers: [AppService],
 })
