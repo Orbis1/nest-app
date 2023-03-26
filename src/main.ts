84bf17e820568b9666ef98aace6bdc1930da6eda
orbis <infectores@gmail.com>
2023-03-26 18:15:08 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app settings
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +17,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  // documentation
+  const config = new DocumentBuilder()
+    .setTitle('Coffees example')
+    .setDescription('The coffee API description')
+    .setVersion('1.0')
+    .addTag('coffee')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
