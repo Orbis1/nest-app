c8469917d056784179ec286ec42d4497d2cc7d0f
orbis <infectores@gmail.com>
2023-03-27 16:19:27 +0300

    encapsulation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { PostgresDataSource } from './app.datasource';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot(PostgresDataSource.options)],
+  imports: [CoffeesModule, TypeOrmModule.forRoot(PostgresDataSource.options), CoffeeRatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
